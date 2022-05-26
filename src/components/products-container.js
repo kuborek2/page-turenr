8ec6bb385f68c7a8c419cb3e1a86ee01c98b3cb4
Jakub Krezolek <kuborek2@gmail.com>
2022-05-27 00:24:17 +0200

    Fisnihed styling product-container component

--- src/components/products-container.js
+++ src/components/products-container.js
@@ -27,6 +27,50 @@ export const ProductsContainer = () => {
                     </Card.Text>
                 </Card.Body>
             </Card>
+            <Card style={{ width: '18rem', backgroundColor: 'yellow' }}>
+                <Card.Body>
+                    <Card.Title>Item id: 5</Card.Title>
+                    <Card.Text>
+                        Name: something
+                    </Card.Text>
+                    <Card.Text>
+                        year: 2006
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+            <Card style={{ width: '18rem', backgroundColor: 'yellow' }}>
+                <Card.Body>
+                    <Card.Title>Item id: 5</Card.Title>
+                    <Card.Text>
+                        Name: something
+                    </Card.Text>
+                    <Card.Text>
+                        year: 2006
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+            <Card style={{ width: '18rem', backgroundColor: 'yellow' }}>
+                <Card.Body>
+                    <Card.Title>Item id: 5</Card.Title>
+                    <Card.Text>
+                        Name: something
+                    </Card.Text>
+                    <Card.Text>
+                        year: 2006
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+            <Card style={{ width: '18rem', backgroundColor: 'yellow' }}>
+                <Card.Body>
+                    <Card.Title>Item id: 5</Card.Title>
+                    <Card.Text>
+                        Name: something
+                    </Card.Text>
+                    <Card.Text>
+                        year: 2006
+                    </Card.Text>
+                </Card.Body>
+            </Card>
       </div>
     );
 }
